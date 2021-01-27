0dee1969625e8cb61b40fc5bb9fee78698358624
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-01-27 23:11:29 +0100

    CarCreate form validation

--- Project_CarRentals/Project_CarRentals/Views/CarCreate.xaml.cs
+++ Project_CarRentals/Project_CarRentals/Views/CarCreate.xaml.cs
@@ -31,6 +31,130 @@ namespace Project_CarRentals.Views
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var context = new CarRentalEntities();
+            successMessage.Text = "";
+
+            string availability = availabilityInput.Text;
+            if (availability == "" || availability == "Availability")
+            {
+                errorMessage.Text = "Enter valid availability!";
+                return;
+            }
+            if(availability != "Yes" && availability != "No")
+            {
+                errorMessage.Text = "Availability must be Yes or No!";
+                return;
+            }
+
+            string brand = brandInput.Text;
+            if (brand == "" || brand == "Brand")
+            {
+                errorMessage.Text = "Enter valid brand!";
+                return;
+            }
+            string model = modelInput.Text;
+            if (model == "" || model == "Model")
+            {
+                errorMessage.Text = "Enter valid model!";
+                return;
+            }
+            if (!carYearInput.Text.All(char.IsDigit)) {
+                errorMessage.Text = "Car year must be a number!";
+                return;
+            }
+            var carYear = int.Parse(carYearInput.Text);
+            if (carYear > DateTime.Now.Year)
+            {
+                errorMessage.Text = "Invalid date, car year cannot be higher than actual year!";
+                return;
+            }
+            if (carYear < 1870)
+            {
+                errorMessage.Text = "Invalid date, car year must be higher than car invention year!";
+                return;
+            }
+            string fuelType = fuelTypeInput.Text.ToLower();
+            if (fuelType == "" || fuelType == "fuel type" )
+            {
+                errorMessage.Text = "Enter valid fuel type!";
+                return;
+            }
+            if(fuelType != "petrol" && fuelType != "gas" && fuelType != "diesel")
+            {
+                errorMessage.Text = "Fuel type must be gas or diesel or petrol!";
+                return;
+            }
+
+            if (!mileageInput.Text.All(char.IsDigit))
+            {
+                errorMessage.Text = "Mileage must be a number!";
+                return;
+            }
+            var mileage = int.Parse(mileageInput.Text);
+            if (mileage < 0)
+            {
+                errorMessage.Text = "Mileage cannot be negative!";
+                return;
+            }
+
+            string type = typeInput.Text;
+            if (type == "" || type == "Type")
+            {
+                errorMessage.Text = "Enter type!";
+                return;
+            }
+
+            try
+            {
+
+                var pricePerKm = decimal.Parse(pircePerKmInput.Text);
+                if(pricePerKm <= 0)
+                {
+                    errorMessage.Text = "Price per kilometer must be higher than zero!";
+                    return;
+                }
+            }
+            catch
+            {
+                errorMessage.Text = "Invalid price per kilometer!";
+                return; 
+            }
+            try
+            {
+
+                var pricePerHour = decimal.Parse(pircePerHourInput.Text);
+                if (pricePerHour <= 0)
+                {
+                    errorMessage.Text = "Price per hour must be higher than zero!";
+                    return;
+                }
+            }
+            catch
+            {
+                errorMessage.Text = "Invalid price per hour!";
+                return;
+            }
+            try
+            {
+
+                var pricePerDay = decimal.Parse(pircePerDayInput.Text);
+                if (pricePerDay <= 0)
+                {
+                    errorMessage.Text = "Price per day must be higher than zero!";
+                    return;
+                }
+            }
+            catch
+            {
+                errorMessage.Text = "Invalid price per day!";
+                return;
+            }
+
+            string userId = (userIDSelect.SelectedValue.ToString());
+            if(userId == "")
+            {
+                errorMessage.Text = "Choose user!";
+                return;
+            }
             var newCar = new Cars()
             {
                 Availability = availabilityInput.Text,
@@ -47,6 +171,9 @@ namespace Project_CarRentals.Views
             };
             context.Cars.Add(newCar);
             context.SaveChanges();
+
+            errorMessage.Text = "";
+            successMessage.Text = "Car was created.";
         }
         /// <summary>
         /// User selection from the list of users from DataBase
